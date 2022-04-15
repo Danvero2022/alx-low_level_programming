d57ab7d6287f7b5c0341ddef1092d4b84a884241
Danvero2022 <99843749+Danvero2022@users.noreply.github.com>
2022-04-15 17:21:52 +0100

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,40 +1,23 @@
 #include "3-calc.h"
 
 /**
- * main - arguments for struct
- * @argc: numbers arguments
- * @argv: string arguments
- * Return: 0.
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no errors
  */
-
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int (*d)(int, int);
+	register int a, b;
+	int (*fptr)(int, int);
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	d = get_op_func(argv[2]);
-		if (d == NULL)
-		{
-			printf("Error\n");
-			exit(99);
-		}
-
-
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
-
-	printf("%d\n", d(i, j));
-
+		printf("Error\n"), exit(98);
+	fptr = get_op_func(argv[2]);
+	if (!fptr)
+		printf("Error\n"), exit(99);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%i\n", fptr(a, b));
 	return (0);
 }
